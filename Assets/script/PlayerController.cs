ba3c526052a25d232eed1cee736ca8fcd211eae9
NastyaChu <159691959+NastyaChu@users.noreply.github.com>
2024-02-11 21:54:05 +0300

    jamp

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpForce;
+
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+            _fallVelocity -= jumpForce;
+        }
+
+    }
+    void FixedUpdate()
+    {
+        
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedUnscaledDeltaTime);
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+
+
+        }
+}
