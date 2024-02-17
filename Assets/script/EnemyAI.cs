bcd1028aa93a99ce86b34ec423a9de6fba9e62db
NastyaChu <159691959+NastyaChu@users.noreply.github.com>
2024-02-17 18:11:59 +0300

    Enemy

---
+++ Assets/script/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+
+    
+
+    private NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        PickNewPatrolPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+}
