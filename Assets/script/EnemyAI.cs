4ad11cc5366bcdb0577d2a5c0ba14a5a6f777d8a
NastyaChu <159691959+NastyaChu@users.noreply.github.com>
2024-02-18 20:41:56 +0300

    enemy's vision

--- Assets/script/EnemyAI.cs
+++ Assets/script/EnemyAI.cs
@@ -6,30 +6,55 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> patrolPoints;
-
-    
+    public PlayerController player;
+    public float viewAngle;
 
     private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
+        InitComponentLinks();
 
         PickNewPatrolPoint();
     }
 
-    // Update is called once per frame
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+   
     void Update()
     {
-      if (_navMeshAgent.remainingDistance == 0)
+        NoticePlayerUpdate();
+        PatrolUpdate();
+    }
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
         {
-            PickNewPatrolPoint();
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
         }
     }
-
     private void PickNewPatrolPoint()
     {
         _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
     }
 }
