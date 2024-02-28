6618ea8c36cecc1dc0617701d796b2b072fe6c88
NastyaChu <159691959+NastyaChu@users.noreply.github.com>
2024-02-28 20:08:39 +0300

    Direction and damage

---
+++ Assets/script/Magicball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magicball : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float damage = 10;
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        EnemyDamage(collision);
+        DestroyFireball();
+    }
+
+    private void EnemyDamage(Collision collision)
+    {
+        var EnemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (EnemyHealth != null)
+        {
+            EnemyHealth.DealDamage(damage);
+        }
+    }
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+    
+    void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/script/MagicballCaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicballCaster : MonoBehaviour
+{
+    public Magicball magicballPrefab;
+    public Transform magicballSourceTransform;
+     private void Update()
+    {
+        if   (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(magicballPrefab, magicballSourceTransform.position, magicballSourceTransform.rotation);
+        }
+    }
+}
