2d4162ff0197af6012d6f23df1c176b7e8b10eaf
NastyaChu <159691959+NastyaChu@users.noreply.github.com>
2024-03-21 22:08:05 +0300

    bomb

---
+++ Assets/script/Explosion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float damage = 50;
+    public float maxSize = 5;
+    public float speed = 1;
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        transform.localScale += Vector3.one * Time.deltaTime * speed;
+
+        if(transform.localScale.x > maxSize)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var PlayerHealth = other.GetComponent<PlayerHealth>();
+        if(PlayerHealth != null)
+        {
+            PlayerHealth.DealDamage(damage);
+        }
+
+        var EnemyHealth = other.GetComponent<EnemyHealth>();
+        if (EnemyHealth != null)
+        {
+            EnemyHealth.DealDamage(damage);
+        }
+    }
+}
