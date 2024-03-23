e944c63c25ddb351b3f499849153d85d6bf3e795
NastyaChu <159691959+NastyaChu@users.noreply.github.com>
2024-03-23 22:12:58 +0300

    spawn aidkit

---
+++ Assets/script/AidkitSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AidkitSpawner : MonoBehaviour
+{
+    public Aidkit aidkitPrefab;
+    private Aidkit _aidkit;
+    public List<Transform> spawnerPoints;
+
+    public float delayMin = 3;
+    public float delayMax = 9;
+
+    private void Update()
+    {
+        if (_aidkit != null) return;
+        if (IsInvoking()) return;
+        Invoke("CreatAidkit", Random.Range(delayMin, delayMax));
+    }
+    private void CreatAidkit()
+    {
+        _aidkit = Instantiate(aidkitPrefab);
+        _aidkit.transform.position = spawnerPoints[Random.Range(0, spawnerPoints.Count)].position;
+    }
+}
